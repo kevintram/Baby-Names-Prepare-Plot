8381d22494d624ba5e41f8b31ee1323ceb027423
kevintram <thekevintram@gmail.com>
2021-02-09 13:17:34 -0500

    Implements prepare.jl

--- prepare.jl
+++ prepare.jl
@@ -1,13 +1,43 @@
 using Pkg
+
+Pkg.add("SQLite")
+Pkg.add("DataFrames")
+Pkg.add("ZipFile")
+Pkg.add("CSV")
+
 using SQLite
+using DataFrames
 using ZipFile
+using CSV
 
 namesZipFile = ARGS[1]
 dbFile = ARGS[2]
 
-zipReader = ZipFile.Reader(namesZipFile)
-println(zipReader)
-
 db = SQLite.DB(dbFile)
 
-SQLite.execute(db, "SELECT name FROM names;")
+DBInterface.execute(db, 
+"CREATE TABLE names (
+    year INTEGER, 
+    name TEXT,
+    sex TEXT,
+    num INTEGER
+    );"
+)
+
+preparedStatement = DBInterface.prepare(db, "INSERT INTO names VALUES(?, ?, ?, ?)")
+
+zipReader = ZipFile.Reader(namesZipFile)
+
+for file in zipReader.files
+    if (contains(file.name,"yob"))
+        year = file.name[4:7]
+        csv = CSV.File(file; header = ["name", "sex", "num"])
+
+        for row in csv
+            DBInterface.execute(preparedStatement, (year, row.name, row.sex, row.num))
+        end
+    end
+end
+
+DBInterface.close!(preparedStatement)
+close(zipReader)
